279175d998ca07658f0a390a1810c9af6b302d3f
bruno-br <bruno.brasilf@gmail.com>
2021-08-06 00:49:39 -0300

    chore: add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,14 @@
 import "reflect-metadata";
 import express from "express";
+import cors from "cors";
 import { router } from "./routes";
 
 import "./database";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use(router);
 
 app.listen(3333, () => {
