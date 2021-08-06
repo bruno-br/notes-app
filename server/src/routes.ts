b5a42a84466ecc2aa3f318f56fe1130d34de2397
bruno-br <bruno.brasilf@gmail.com>
2021-08-06 00:25:01 -0300

    feat: add delete notes route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { CreateNoteController } from "./controllers/CreateNoteController";
 import { GetNotesController } from "./controllers/GetNotesController";
+import { DeleteNotesController } from "./controllers/DeleteNoteController";
 
 const router = Router();
 
 const getNotesController = new GetNotesController();
 const createNoteController = new CreateNoteController();
+const deleteNoteController = new DeleteNotesController();
 
 router.get("/notes", getNotesController.handle);
 router.post("/notes", createNoteController.handle);
+router.delete("/notes", deleteNoteController.handle);
 
 export { router };
