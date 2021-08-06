7214c56a6c5b2ed4433fa0c1f1d1fbab6e73a4cb
bruno-br <bruno.brasilf@gmail.com>
2021-08-05 22:32:21 -0300

    chore: setup typeorm with sqlite

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,8 @@
+import "reflect-metadata";
 import express from "express";
 import { router } from "./routes";
+
+import "./database";
 
 const app = express();
 
@@ -8,5 +11,5 @@ app.use(express.json());
 app.use(router);
 
 app.listen(3333, () => {
-  console.log("Hello World");
+  console.log("Server is running");
 });
