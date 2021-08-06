193250ee39216994ca97ec5cb28a27d33314558d
bruno-br <bruno.brasilf@gmail.com>
2021-08-06 01:28:36 -0300

    feat: save created note on database

--- web/src/components/NotePad/index.tsx
+++ web/src/components/NotePad/index.tsx
@@ -1,13 +1,29 @@
 import "./styles.css";
+import { api } from "../../services/api";
+import { useState } from "react";
 
 function NotePad() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  function createNote() {
+    api.post("/notes", { title, description });
+  }
+
   return (
     <div className="notepad">
       <h1>Bloco de notas</h1>
-      <input placeholder="Assunto" type="text" />
-      <textarea placeholder="Texto"></textarea>
+      <input
+        placeholder="Assunto"
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        placeholder="Texto"
+        onChange={(e) => setDescription(e.target.value)}
+      ></textarea>
       <div className="btn-container">
-        <button>CRIAR NOTA</button>
+        <button onClick={createNote}>CRIAR NOTA</button>
       </div>
     </div>
   );
